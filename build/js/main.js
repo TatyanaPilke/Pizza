218ef5b81fd33bda5b04120e994d6027954da3fa
TatyanaPilke <kykla.mc2@gmail.com>
2021-03-28 16:34:06 +0300

    added section cook, simplebar

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,11 @@
-// $(function () {
 
-// });
+  
+
+  var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+  
